353b4b33d2aced20f6ec1c52c97cb40a0b3fb997
j-evs <jan.evseev@gmail.com>
2017-10-12 12:16:03 +0300

    Initial bot setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.load();
+
+const Telegraf = require('telegraf');
+const { reply } = Telegraf;
+
+console.log(process.env);
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.command('/oldschool', (ctx) => ctx.reply('Hello'));
+bot.command('/modern', ({ reply }) => reply('Yo'));
+bot.command('/hipster', reply('λ'));
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there!'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.startPolling();
