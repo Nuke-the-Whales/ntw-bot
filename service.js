0447c89145b65671641c2827ff93c1bbad28ca54
Konstantin Levin <klevin@bekitzur.com>
2017-10-16 10:59:40 +0300

    Add temporary service/proxy integration

--- service.js
+++ service.js
@@ -1,12 +1,27 @@
 const fetch = require('node-fetch');
-const API_ENDPOINT = 'http://example.example'
+const API_ENDPOINT = 'http://localhost:8080'
 
 const searchSeries = async (searchInput) => {
     try {
-        const response = fetch(`${API_ENDPOINT}/search`)
+        const response = fetch(`${API_ENDPOINT}/search?title=${searchInput}`)
         const json = await response.json();
     } catch (error) {
         console.log('error searching series', error)
+    }
+
+    if (json) {
+        return json;
+    }
+
+    return {error: true};
+}
+
+const showItem = async (itemId) => {
+    try {
+        const response = fetch(`${API_ENDPOINT}/show?id=${itemId}`)
+        const json = await response.json();
+    } catch (error) {
+        console.log('error showing series', error)
     }
 
     if (json) {
