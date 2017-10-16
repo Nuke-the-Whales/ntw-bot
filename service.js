7dcc9587bb152430bde6a91cc9ac1f690d2d7027
j-evs <jan.evseev@gmail.com>
2017-10-16 17:36:37 +0300

    search works

--- service.js
+++ service.js
@@ -3,48 +3,37 @@ const API_ENDPOINT = 'http://localhost:8080'
 
 const searchSeries = async (searchInput) => {
     try {
-        const response = fetch(`${API_ENDPOINT}/search?title=${searchInput}`)
+        const response = await fetch(`${API_ENDPOINT}/search?title=${searchInput}`);
         const json = await response.json();
-    } catch (error) {
-        console.log('error searching series', error)
-    }
-
-    if (json) {
         return json;
+    } catch (error) {
+        console.log('error searching series', error);
+        return {error: true}
     }
-
-    return {error: true};
-}
+};
 
 const showItem = async (itemId) => {
     try {
-        const response = fetch(`${API_ENDPOINT}/show?id=${itemId}`)
+        const response = await fetch(`${API_ENDPOINT}/show?id=${itemId}`);
         const json = await response.json();
-    } catch (error) {
-        console.log('error showing series', error)
-    }
-
-    if (json) {
         return json;
+    } catch (error) {
+        console.log('error showing series', error);
+	    return {error: true};
     }
-
-    return {error: true};
-}
+};
 
 const addSubscription = async (userId, seriesId) => {
     try {
         const response = fetch(`${API_ENDPOINT}`, { method: 'POST', body: {userId, seriesId} })
         const json = await response.json();
+        return json;
     } catch (error) {
-        console.log('error adding subscription', error);
+	    console.log('error adding subscription', error);
+	    return {error: true}
     }
-
-    if (json) {
-        return json
-    }
-
-    return {error: true}
-}
+};
 
 module.exports.searchSeries = searchSeries;
+module.exports.showItem = showItem;
 module.exports.addSubscription = addSubscription;
