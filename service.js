a9833e5c699fc60e9c3775ae7786346cc1e904ed
j-evs <jan.evseev@gmail.com>
2017-10-17 19:29:41 +0300

    add inline stuff

--- service.js
+++ service.js
@@ -13,6 +13,7 @@ const searchSeries = async (searchInput) => {
 };
 
 const showItem = async (itemId) => {
+    console.log('showItem')
     try {
         const response = await fetch(`${API_ENDPOINT}/show?id=${itemId}`);
         const json = await response.json();
@@ -24,7 +25,9 @@ const showItem = async (itemId) => {
 };
 
 const addSubscription = async (userId, seriesId) => {
-    // try {
+	const response = await fetch(`${API_ENDPOINT}/subscriptions`);
+
+	// try {
     //     const response = fetch(`${API_ENDPOINT}`, { method: 'POST', body: {userId, seriesId} })
     //     const json = await response.json();
     //     return json;
